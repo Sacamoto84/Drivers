9fac8b59141bcac9037ff0b3f67e54547af93d53
zorromen <zorromen@mail.ru>
2023-05-13 09:56:38 +0300

    V77

--- libs/DWT.h
+++ libs/DWT.h
@@ -1,12 +1,31 @@
-#include "DWT.h"
+/*
+ * DWT.h
+ *
+ *  Created on: 4 февр. 2021 г.
+ *      Author: Ivan
+ */
 
-void DWT_Init(void)
+#ifndef DWT_H_
+#define DWT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+
+#define    DWT_CYCCNT    *(volatile uint32_t*)0xE0001004
+#define    DWT_CONTROL   *(volatile uint32_t*)0xE0001000
+#define    SCB_DEMCR     *(volatile uint32_t*)0xE000EDFC
+
+
+static inline void DWT_Init(void)
 {
     SCB_DEMCR |= CoreDebug_DEMCR_TRCENA_Msk; // разрешаем использовать счётчик
     DWT_CONTROL |= DWT_CTRL_CYCCNTENA_Msk;   // запускаем счётчик
 }
 
-void delay_micros(uint32_t us)
+static inline void delay_micros(uint32_t us)
 {
     uint32_t us_count_tic =  us * (SystemCoreClock / 1000000); // получаем кол-во тактов за 1 мкс и умножаем на наше значение
     DWT->CYCCNT = 0U; // обнуляем счётчик
@@ -17,3 +36,9 @@ void delay_micros(uint32_t us)
 //DWT_CYCCNT = 0;// обнуляем счётчик
 // здесь кусок измеряемого участка программы
 //count_tic = DWT_CYCCNT; // кол-во тактов
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DWT_H_ */
